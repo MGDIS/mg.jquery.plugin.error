d30d5bc771894feae59faac0907b50821e7cbf30
Alban Mouton <alban.mouton@gmail.com>
2013-05-22 12:46:10 +0300

    Change licence to LGPL3

--- tests/unit/error/error.js
+++ tests/unit/error/error.js
@@ -1,3 +1,22 @@
+  // mg.jquery.plugin.error, a jquery-ui widget to help you manage Javascript errors in your users' browsers.
+  // Copyright (C) 2013 MGDIS
+  // For details see https://github.com/MGDIS/mg.jquery.plugin.error
+ 
+  // This program is free software: you can redistribute it and/or modify
+  // it under the terms of the GNU Lesser General Public License as published by
+  // the Free Software Foundation, either version 3 of the License, or
+  // (at your option) any later version.
+ 
+  // This program is distributed in the hope that it will be useful,
+  // but WITHOUT ANY WARRANTY; without even the implied warranty of
+  // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  // GNU General Public License for more details.
+ 
+  // You should have received a copy of the GNU General Public License
+  // along with this program. If not, see <http://www.gnu.org/licenses/>.
+ 
+  // This is free software, and you are welcome to redistribute it
+  // under certain conditions; for details see <http://www.gnu.org/licenses/>.
 
 function prepareErrorDiv(){
 	return $("<div></div>").appendTo($("#qunit-fixture"));
